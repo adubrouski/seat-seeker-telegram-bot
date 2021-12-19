4f60ea4b86e2684b4da6d38174b9d5563cfda52b
Anton Dubrouski <adubrouski145@gmail.com>
2021-12-19 21:26:08 +0300

    refactor: change API_URL

--- src/queries/html.query.ts
+++ src/queries/html.query.ts
@@ -1,7 +1,7 @@
 import https from 'https';
 
 const API_URL =
-  'https://atlasbus.by/%D0%9C%D0%B0%D1%80%D1%88%D1%80%D1%83%D1%82%D1%8B/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA/%D0%93%D1%80%D0%BE%D0%B4%D0%BD%D0%BE?date=2021-12-18&passengers=1&from=c625144&to=c627904';
+  'https://atlasbus.by/%D0%9C%D0%B0%D1%80%D1%88%D1%80%D1%83%D1%82%D1%8B/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA/%D0%93%D1%80%D0%BE%D0%B4%D0%BD%D0%BE?date=2021-12-20&passengers=1&from=c625144&to=c627904';
 
 export const getHtmlContent = (): Promise<string> => {
   return new Promise((resolve, reject) => {
