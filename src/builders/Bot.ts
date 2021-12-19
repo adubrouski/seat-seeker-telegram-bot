4b4782b92ba4e4cbcefdbb7e9a2b8aabf640b2e9
Anton Dubrouski <adubrouski145@gmail.com>
2021-12-19 21:30:49 +0300

    refactor: add Bot class, add connection error handling

---
+++ src/builders/Bot.ts
@@ -0,0 +1,38 @@
+import TelegramBot from 'node-telegram-bot-api';
+import { TelegramConnectionError } from '../errors/HttpError';
+
+export class Bot {
+  public static instance: TelegramBot;
+
+  constructor(
+    private apiToken: string,
+    private config: TelegramBot.ConstructorOptions,
+  ) {}
+
+  public async start() {
+    try {
+      const bot = new TelegramBot(this.apiToken, this.config);
+
+      await Bot.checkConnection(bot);
+
+      Bot.instance = bot;
+
+      return bot;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private static async checkConnection(bot: TelegramBot) {
+    try {
+      await bot.getMe();
+
+      return true;
+    } catch (error) {
+      throw new TelegramConnectionError('ConnectionError', {
+        status: error?.response?.statusCode ?? 500,
+        statusText: error?.response?.statusMessage ?? 'Internal server error',
+      });
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
-import TelegramBot from 'node-telegram-bot-api';
 import appConfig from '../appconfig.json';
 import 'dayjs/locale/ru';
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import { StartController } from './modules/start/start.controller';
-import { matchQueryController } from './middleware/callback-query.middleware';
-import { BotBuilder } from './builders/BotBuilder';
+import { Bot } from './builders/Bot';
+import { TelegramConnectionError } from './errors/HttpError';
+import { routes } from './routes';
 
 dayjs.extend(localizedFormat);
 dayjs.locale('ru');
 
 (async () => {
   try {
-    const bot = await new BotBuilder()
-      .setApiToken(appConfig['API-KEY'])
-      .setConfig({ polling: true })
-      .build();
+    const bot = await new Bot(appConfig.API_KEY, appConfig.BOT_CONFIG).start();
 
-    bot.onText(/\/start/, new StartController().getCitiesKeyboard);
-    bot.on('callback_query', matchQueryController);
+    console.log(`BOT IS RUNNING`);
+
+    bot.onText(new RegExp('/start'), (message) => {
+      new StartController(message.chat.id, null).start();
+    });
+    bot.on('callback_query', routes);
   } catch (error) {
+    if (error instanceof TelegramConnectionError) {
+      console.log(error.details.status, error.details.statusText);
+    } else {
+      console.log('Unknown error');
+    }
     process.exit(1);
   }
 })();
