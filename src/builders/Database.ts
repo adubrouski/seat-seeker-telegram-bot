bacb0e1cee90c80e1225884ba90afec5ff7b5f47
Anton Dubrouski <adubrouski145@gmail.com>
2021-12-21 23:46:37 +0300

    feat: create database connection class

---
+++ src/builders/Database.ts
@@ -0,0 +1,25 @@
+import mysql, { Connection, ConnectionOptions } from 'mysql2/promise';
+import { ConnectionError } from '../errors/connection.error';
+
+export class Database {
+  constructor(private options: ConnectionOptions) {}
+
+  public static connection: Connection;
+
+  public connect() {
+    return mysql
+      .createConnection(this.options)
+      .then((connection) => {
+        Database.connection = connection;
+        return connection.config.database!;
+      })
+      .catch((error) => {
+        return Promise.reject(
+          new ConnectionError('DatabaseConnectionError', {
+            code: error?.errno ?? 999,
+            description: error?.code ?? 'Unknown error',
+          }),
+        );
+      });
+  }
+}
