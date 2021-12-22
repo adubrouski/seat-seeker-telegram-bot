ec81b8d58df6727bf6c8ad6d79d943fa764585aa
Anton Dubrouski <adubrouski145@gmail.com>
2021-12-22 23:23:07 +0300

    refactor: create launch folder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import 'dayjs/locale/ru';
 import dayjs from 'dayjs';
-import { launch } from './services/launch.service';
+import { launch } from './launch/launch';
 
 dayjs.extend(localizedFormat);
 dayjs.locale('ru');
