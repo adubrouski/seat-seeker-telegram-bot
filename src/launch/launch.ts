712fd6eb75e2ee32c81d668517535c4aca5f9196
Anton Dubrouski <adubrouski145@gmail.com>
2021-12-22 23:22:56 +0300

    refactor: create launch folder

--- src/launch/launch.ts
+++ src/launch/launch.ts
@@ -1,15 +1,17 @@
 import appConfig from '../../appconfig.json';
-import { Bot } from '../builders/Bot';
+import { BotConnection } from '../connections/bot.connection';
 import { StartController } from '../modules/start/start.controller';
 import { router } from '../router';
-import { Database } from '../builders/Database';
+import { DatabaseConnection } from '../connections/database.connection';
 import { ConnectionError } from '../errors/connection.error';
 import { START_REGEXP } from '../constants/constants';
 
 export const launch = async () => {
   try {
     /* TELEGRAM API CONNECTION */
-    const { bot, botName } = await new Bot(appConfig.telegramApiConfig).start();
+    const { bot, botName } = await new BotConnection(
+      appConfig.telegramApiConfig,
+    ).start();
 
     bot.onText(START_REGEXP, ({ chat }) => {
       return new StartController(chat.id, null).start();
@@ -19,7 +21,9 @@ export const launch = async () => {
     console.log('CONNECT TO TELEGRAM API - SUCCESS');
 
     /* DATABASE CONNECTION */
-    const databaseName = await new Database(appConfig.databaseConfig).connect();
+    const databaseName = await new DatabaseConnection(
+      appConfig.databaseConfig,
+    ).connect();
 
     console.log(`CONNECT TO ${databaseName} DATABASE - SUCCESS`);
     console.log(`BOT ${botName} IS RUNNING`);
