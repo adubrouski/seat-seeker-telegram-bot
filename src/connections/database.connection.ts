50d01acca88a38707cb29e138378a6ac1d87802d
Anton Dubrouski <adubrouski145@gmail.com>
2021-12-24 00:07:55 +0300

    feat: add creating initial tables

--- src/connections/database.connection.ts
+++ src/connections/database.connection.ts
@@ -1,4 +1,5 @@
 import mysql, { Connection, ConnectionOptions } from 'mysql2/promise';
+import appConfig from '../../appconfig.json';
 import { ConnectionError } from '../errors/connection.error';
 
 export class DatabaseConnection {
@@ -11,15 +12,40 @@ export class DatabaseConnection {
       .createConnection(this.options)
       .then((connection) => {
         DatabaseConnection.connection = connection;
+
+        return connection;
+      })
+      .then(async (connection) => {
+        await this.init(connection);
+
         return connection.config.database!;
       })
-      .catch((error) => {
-        return Promise.reject(
+      .catch((error) =>
+        Promise.reject(
           new ConnectionError('DatabaseConnectionError', {
-            code: error?.errno ?? 999,
-            description: error?.code ?? 'Unknown error',
+            code: error.errno ?? 999,
+            description: error.code ?? 'Unknown error',
           }),
-        );
-      });
+        ),
+      );
+  }
+
+  public async init(connection: Connection) {
+    try {
+      await connection.query(
+        'CREATE TABLE IF NOT EXISTS `cities` (id VARCHAR(7) NOT NULL, name VARCHAR(12) NOT NULL, PRIMARY KEY (`id`))',
+      );
+      const [cities]: any = await DatabaseConnection.connection.query(
+        'SELECT * FROM `cities`',
+      );
+
+      if (!cities.length) {
+        await connection.query('INSERT INTO `cities` (id, name) VALUES ?', [
+          appConfig.cities,
+        ]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
