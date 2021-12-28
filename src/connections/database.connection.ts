aa2cb7b4e5788d893ebe824f417e3f1e0d8c3760
Anton Dubrouski <adubrouski145@gmail.com>
2021-12-28 16:18:12 +0300

    refactor: remove comments

--- src/connections/database.connection.ts
+++ src/connections/database.connection.ts
@@ -11,85 +11,45 @@ export class DatabaseConnection {
   public async connect() {
     const knex = createKnexConnection(this.config);
     DatabaseConnection.connection = knex;
-    try {
-      this.createInitialTables(knex);
-
-      // gg('test')
-      //   .insert({ id: uuid4(), name: 'gergererg' })
-      //   .then((res) => console.log(res))
-      //   .catch((error) => {
-      //     console.log(error);
-      //   });
-      // console.log(ggg);
-    } catch (error) {
-      console.log(error);
-    }
-
-    // return mysql
-    //   .createConnection(this.options)
-    //   .then((connection) => {
-    //     DatabaseConnection.connection = connection;
-    //
-    //     return connection;
-    //   })
-    //   .then(async (connection) => {
-    //     await this.init(connection);
-    //
-    //     return connection.config.database!;
-    //   })
-    //   .catch((error) =>
-    //     Promise.reject(
-    //       new ConnectionError('DatabaseConnectionError', {
-    //         code: error.errno ?? 999,
-    //         description: error.code ?? 'Unknown error',
-    //       }),
-    //     ),
-    //   );
+    await this.createInitialTables(knex);
   }
 
   public async createInitialTables(knex: Knex) {
-    const isCitiesTableExists = await knex.schema.hasTable('cities');
-    const isUsersTableExists = await knex.schema.hasTable('users');
-    const isSettingsTableExists = await knex.schema.hasTable('settings');
-    console.log(await knex('settings').select());
-    console.log(
-      await knex('settings as s')
-        .join('users as u', 'u.id', 's.user_id')
-        .join('cities as ct', 'ct.id', 's.city_from')
-        .join('cities as cf', 'cf.id', 's.city_to')
-        .select(
-          'cf.name as cityFrom',
-          'ct.name as cityTo',
-          'u.name as username',
-        ),
-    );
-    if (!isCitiesTableExists) {
-      await knex.schema.createTable('cities', (table) => {
-        table.string('id').primary();
-        table.string('name', 30);
-      });
+    try {
+      const isCitiesTableExists = await knex.schema.hasTable('cities');
+      const isUsersTableExists = await knex.schema.hasTable('users');
+      const isSettingsTableExists = await knex.schema.hasTable('settings');
 
-      await knex('cities').insert(appConfig.cities);
-    }
+      if (!isCitiesTableExists) {
+        await knex.schema.createTable('cities', (table) => {
+          table.string('id', 7).primary();
+          table.string('name', 30).notNullable();
+        });
 
-    if (!isUsersTableExists) {
-      await knex.schema.createTable('users', (table) => {
-        table.integer('id').primary();
-        table.string('name', 30);
-      });
-    }
+        await knex('cities').insert(appConfig.cities);
+      }
 
-    if (!isSettingsTableExists) {
-      await knex.schema.createTable('settings', (table) => {
-        table.increments('id');
-        table.integer('user_id');
-        table.string('city_from', 7);
-        table.string('city_to', 7);
+      if (!isUsersTableExists) {
+        await knex.schema.createTable('users', (table) => {
+          table.integer('id').primary();
+          table.string('name', 30);
+        });
+      }
 
-        table.foreign('user_id').references('id').inTable('users');
-        table.foreign('city_from').references('id').inTable('cities');
-        table.foreign('city_to').references('id').inTable('cities');
-      });
+      if (!isSettingsTableExists) {
+        await knex.schema.createTable('settings', (table) => {
+          table.increments('id').primary();
+          table.integer('user_id').notNullable();
+          table.string('city_from', 7).notNullable();
+          table.string('city_to', 7).notNullable();
+
+          table.foreign('user_id').references('id').inTable('users');
+          table.foreign('city_from').references('id').inTable('cities');
+          table.foreign('city_to').references('id').inTable('cities');
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 }
