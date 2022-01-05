fdf2911db213950de55fa159f257115a9fde1141
Anton Dubrouski <adubrouski145@gmail.com>
2022-01-05 16:55:20 +0300

    feat: add settings router to app

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { CallbackQuery } from 'node-telegram-bot-api';
 import { useStartRouter } from './start.router';
 import { queryDataParser } from '../../lib/query-data-parser';
+import { useSettingsRouters } from './settings.router';
 
 export const router = async (query: CallbackQuery) => {
   if (!query.data || !query.message) return;
 
   if (queryDataParser(query.data).parseController() === 'start') {
     useStartRouter(query);
+    useSettingsRouters(query);
   }
 };
